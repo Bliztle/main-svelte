9a6aff3bbf5888ef00118ce42e0916d28111c848
bliztle <asbjoern.r.e@gmail.com>
2023-07-20 22:35:45 +0200

    app container

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,11 @@
 	import '../app.css';
 </script>
 
-<!-- <nav>
-	<a href="/">Home</a>
-	<a href="/lotify">Lotify</a>
-</nav> -->
-
-<NavBar />
-
-<slot />
+<div class="h-screen w-screen flex flex-col">
+	<div class="flex-none">
+		<NavBar />
+	</div>
+	<div class="flex-1">
+		<slot />
+	</div>
+</div>

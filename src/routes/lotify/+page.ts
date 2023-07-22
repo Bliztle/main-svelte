9f527fd3d97208761e157ca0410f7d78402f2540
bliztle <asbjoern.r.e@gmail.com>
2023-07-23 01:14:47 +0200

    removed load time

--- src/routes/lotify/+page.ts
+++ src/routes/lotify/+page.ts
@@ -8,8 +8,6 @@ export const load: PageLoad = async () => {
         distance: 10000
     }));
 
-    await new Promise(resolve => setTimeout(resolve, 2000));
-
     return {
         gridData
     };

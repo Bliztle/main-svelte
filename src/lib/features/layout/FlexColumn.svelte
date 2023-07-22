702cc2a2b0bc99bbad41ba78d24a9b8b1be58cb4
bliztle <asbjoern.r.e@gmail.com>
2023-07-22 23:42:06 +0200

    Extraced flex logic into component

---
+++ src/lib/features/layout/FlexColumn.svelte
@@ -0,0 +1,11 @@
+<div class="h-screen w-screen flex flex-col">
+	<div class="flex-none">
+		<slot name="header" />
+	</div>
+	<div class="flex-1">
+		<slot />
+	</div>
+	<div class="flex-none">
+		<slot name="footer" />
+	</div>
+</div>

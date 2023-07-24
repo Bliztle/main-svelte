a739d4f09738cc39650fb4a6e313b3eaf75da185
bliztle <asbjoern.r.e@gmail.com>
2023-07-24 11:21:41 +0200

    missed in last commit

--- src/lib/utility/types/objects.ts
+++ src/lib/utility/types/objects.ts
@@ -1,3 +1,5 @@
 export type ValueOf<T> = T[keyof T];
 
-export type Indexable<T> = Record<keyof T, ValueOf<T>>;
+export type Indexable<T> = T & Record<keyof T, ValueOf<T>>;
+
+export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
